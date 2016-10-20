7996e0585a842939c053747b43e2eda8b9d86a6f
Agradeep Khanra <agradeep92@gmail.com>
2016-10-20 01:28:08 -0400

    Final commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: '5R6BNbw4tJGx5HjvUJMBTOkIL',
+    consumer_secret: 'ko57epn7Cm3nasOIeCuVSRAyVwV0QsECmRMaTpLswbop2UasqE',
+    access_token_key: '3545772197-JcFwaTGinrpr24QTDoZhv2F1qtX0QBd7vuN5vQf',
+    access_token_secret: 'YghzhNmoE2NhuLxLjrVniMMuHuP5hjQYy7NH4qQLHepFH'
+  }
+}
